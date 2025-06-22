4f7e700354e5fcd0d16fb9896e8eb02e01b16433
Bakhtiar_Aleem_Victus <bakhtiar.aleem93@gmail.com>
2025-06-23 00:05:56 +0500

    conmmenting and setting up things

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,5 @@
+
+// Set Active Class
 function setActive(button) {
     document.querySelectorAll('.chat-item').forEach(items => {
         items.classList.remove('active');
@@ -5,8 +7,9 @@ function setActive(button) {
     })
 }
 
+// Get Htmx request Events
 document.body.addEventListener('htmx:configRequest', function (event) {
-    if(event.detail.target.id === 'chatMessage'){
+    if (event.detail.target.id === 'chatMessage') {
         const sendForm = document.getElementById('chat-submit');
         const chatWelcomeScreen = document.getElementById('chat-welcome');
         sendForm.style.display = "block";
@@ -36,63 +39,94 @@ document.body.addEventListener('htmx:configRequest', function (event) {
 })
 
 
-
-document.body.addEventListener('htmx:confirm', function(evt) {
-  if (evt.detail.path.startsWith('/delete-room') && evt.detail.verb === "delete") {
-    evt.preventDefault();
-    swal({
-      title: "Are you sure?",
-      text: evt.detail.question,
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    }).then((confirmed) => {
-      if (confirmed) {
-        // evt.detail.issueRequest();
-        const url = new URL('http://mock.com' + evt.detail.path)
-        const id = url.searchParams.get('id')
-        const currentID = document.getElementById(id)
-        currentID.remove();
-      }
-    });
-  }
+// Get Htmx confirm Events
+document.body.addEventListener('htmx:confirm', function (evt) {
+    if (evt.detail.path.startsWith('/delete-room') && evt.detail.verb === "delete") {
+        evt.preventDefault();
+        Swal.fire({
+            title: "Are you sure?",
+            text: evt.detail.question,
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+            preConfirm: () => {
+                // evt.detail.issueRequest();
+                const url = new URL('http://mock.com' + evt.detail.path)
+                const id = url.searchParams.get('id')
+                const currentID = document.getElementById(id)
+                currentID.remove();
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+        })
+    }
+    if (evt.detail.path.startsWith('/edit-room') && evt.detail.verb === "put") {
+        evt.preventDefault();
+        Swal.fire({
+            title: evt.detail.question,
+            input: "text",
+            inputAttributes: {
+                autocapitalize: "off"
+            },
+            showCancelButton: true,
+            confirmButtonText: "Change Name",
+            showLoaderOnConfirm: true,
+            preConfirm: (nameChange) => {
+                const url = new URL('http://mock.com' + evt.detail.path);
+                const id = url.searchParams.get('id');
+                const currentID = document.getElementById(id);
+                if (currentID) {
+                    const element = currentID.querySelector('.chat-name')
+                    if (element) {
+                        element.innerText = nameChange;
+                    }
+                }
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: `Room Name Changed`,
+                });
+            }
+        });
+    }
 });
 
 
-function inviteFunc(name){
-    swal({
-      title: "Are you sure you want to join this Chat Room?",
-      icon: "success",
-      buttons: true,
-    }).then((confirmed) => {
-      if (confirmed) {
-        const joinChatRoom = document.getElementById('chat-room-joined');
-        const invite = document.getElementById(name)
-        const clone = invite.cloneNode(true)
-        const tags = clone.querySelector('.chat-item-clickable')
-        if(tags){
-            tags.setAttribute('hx-get', `./mockapi/${name}.html`);
-            tags.setAttribute('hx-target', '#chatMessage');
-            tags.setAttribute('hx-swap', 'innerHTML');
-        }
-        const button = clone.querySelector("button");
-        if(button){
-            button.remove();
-        }
+// invite funtion using HTMX on click
+function inviteFunc(name) {
+    Swal.fire({
+        title: "Are you sure you want to join this Chat Room?",
+        icon: "success",
+        buttons: true,
+        preConfirm: () => {
+            const joinChatRoom = document.getElementById('chat-room-joined');
+            const invite = document.getElementById(name)
+            const clone = invite.cloneNode(true)
+            const tags = clone.querySelector('.chat-item-clickable')
+            if (tags) {
+                tags.setAttribute('hx-get', `./mockapi/${name}.html`);
+                tags.setAttribute('hx-target', '#chatMessage');
+                tags.setAttribute('hx-swap', 'innerHTML');
+            }
+            const button = clone.querySelector("button");
+            if (button) {
+                button.remove();
+            }
 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.className = 'delete';
-        deleteBtn.setAttribute('hx-delete', `/delete-room?id=${name}`);
-        deleteBtn.setAttribute('hx-confirm', 'Are you sure? You want to delete this chat');
-        deleteBtn.innerHTML = `<svg>
+            const deleteBtn = document.createElement('button');
+            deleteBtn.className = 'delete';
+            deleteBtn.setAttribute('hx-delete', `/delete-room?id=${name}`);
+            deleteBtn.setAttribute('hx-confirm', 'Are you sure? You want to delete this chat');
+            deleteBtn.innerHTML = `<svg>
             <path d="M6 7V18C6 19.1046 6.89543 20 8 20H16C17.1046 20 18 19.1046 18 18V7M6 7H5M6 7H8M18 7H19M18 7H16M10 11V16M14 11V16M8 7V5C8 3.89543 8.89543 3 10 3H14C15.1046 3 16 3.89543 16 5V7M8 7H16" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>`;
 
-        clone.appendChild(deleteBtn)
-        joinChatRoom.appendChild(clone)
-        htmx.process(clone);
-        invite.remove();
-      }
-    });
-
+            clone.appendChild(deleteBtn)
+            joinChatRoom.appendChild(clone)
+            htmx.process(clone);
+            invite.remove();
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+    })
 }
